572acfaef168682d6e51da30f5af966c7960ad6e
Nik Reist <auric.magi@gmail.com>
2019-04-15 23:51:30 -0400

    Fixed spacing, and file and folder checking

--- get_murmur-deps.sh
+++ get_murmur-deps.sh
@@ -6,29 +6,35 @@
 # <http://mumble.info/mumble-releng-experimental/LICENSE>.
 
 case "$OSTYPE" in
-		msys* ) triplet='x64-windows-static';;
-		linux-gnu* ) triplet='x64-linux';;
-		darwin* ) triplet='x64-osx';;
-		* ) echo "The OSTYPE is either not defined or unsupported. Aborting...";;
+   msys* ) triplet='x64-windows-static';;
+   linux-gnu* ) triplet='x64-linux';;
+   darwin* ) triplet='x64-osx';;
+   * ) echo "The OSTYPE is either not defined or unsupported. Aborting...";;
 esac
 
-if [ ! -d './vcpkg' ]; then
-	git clone https://github.com/Microsoft/vcpkg.git ../vcpkg
-	# vcpkg does not have a port for zeroc ice or mcpp, copy homegrown ports 
-	cp -R helpers/vcpkg/ports/* ../vcpkg/ports/
-	if [ $? -eq 0 ]; then
-		cd ../vcpkg
-		case "$OSTYPE" in
-			msys* ) ./bootstrap-vcpkg.bat;;
-			* ) bash bootstrap-vcpkg.sh;;
-		esac
-		./vcpkg integrate install
-		[ -z "$triplet" ] && echo "Triplet type is not defined! Aborting..." || \
-		./vcpkg install qt5-base gRPC boost-atomic boost-function boost-optional \
-			--triplet $triplet
-	else
-		echo "Failed to retrieve the 'vcpkg' repository! Aborting..."
-	fi
+if [ ! -d "../vcpkg" ]
+   then 
+      git clone https://github.com/Microsoft/vcpkg.git ../vcpkg
+      # vcpkg does not have a port for zeroc ice or mcpp, copy homegrown ports 
+      cp -R helpers/vcpkg/ports/* ../vcpkg/ports/
+      if [ "$(ls -A ../vcpkg)" ] 
+	     then
+            cd ../vcpkg
+            case "$OSTYPE" in
+               msys* ) ./bootstrap-vcpkg.bat;;
+               * ) bash bootstrap-vcpkg.sh;;
+            esac
+            ./vcpkg integrate install
+            if [ -z "$triplet" ]
+			   then
+			      echo "Triplet type is not defined! Aborting..."
+			else
+               ./vcpkg install qt5-base gRPC boost-atomic boost-function boost-optional \
+                  --triplet $triplet
+			fi
+      else
+         echo "Failed to retrieve the 'vcpkg' repository! Aborting..."
+      fi
 else
-	echo "vcpkg repository exists! Use vcpkg binary to manage the repository. Aborting..."
+   echo "vcpkg repository exists! Use vcpkg binary to manage the repository. Aborting..."
 fi
