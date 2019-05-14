e619e690f2c97fd0b7b9b89467f624f0df7325f2
Nik Reist <auric.magi@gmail.com>
2019-05-14 00:12:16 -0400

    Corrected version

--- helpers/vcpkg/ports/zeroc-mcpp/portfile.cmake
+++ helpers/vcpkg/ports/zeroc-mcpp/portfile.cmake
@@ -3,7 +3,7 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
    OUT_SOURCE_PATH SOURCE_PATH
    REPO zeroc-ice/mcpp
-   REF v2.7.2.13
+   REF v2.7.2.12
    SHA512 8cfe5e7dc191fa43c9e2b46a99a3489e572a9fab8df6a1937a0ff03a6e9abc4843878eb5481e6bd4f87c9cc87cc3537a988523d31eb2954d55fc216eb3d4860c
    HEAD_REF master
 )
