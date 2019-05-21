05c4e3834101750212d8048de1a59701a5dbc2f7
Nik Reist <auric.magi@gmail.com>
2019-05-21 00:45:12 -0400

    Change some var names for clarity, set file name var, add args to command, and use file name var in comment.

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -1,10 +1,11 @@
 # handle slice building so the projects that need it can call it
-function(CompileSlices slice_bin slice_path ice_file_path output_dir gen_file_type)
+function(CompileSlices slice_bin slice_path ice_file output_dir gen_file_type)
+   get_filename_component(ice_file_FILE_NAME ${ice_file} NAME_WE)
    add_custom_command(
 	  OUTPUT ${gen_file_type}
-	  COMMAND "${slice_bin} -I${slice_path} ${ice_file_path}"
+	  COMMAND ${slice_bin} 
+	  ARGS "-I${slice_path} ${ice_file}"
       WORKING_DIRECTORY ${output_dir}
-	  COMMENT "Generating Ice Sources from ${ice_file_path_FILE_NAME}..."
+	  COMMENT "Generating Ice Sources from ${ice_file_FILE_NAME}..."
    )
-   file(GENERATE OUTPUT "${output_dir}/${ice_file_path_FILE_NAME}" CONTENT ${slice_bin})
 endfunction()
