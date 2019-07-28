22cfd37af01c8ec07ca93ea644a16608c33006fa
Nik Reist <auric.magi@gmail.com>
2019-07-27 23:13:17 -0400

    Add compile options and defs

--- helpers/ice-cmake/cpp/cmake/ice_cpp_props.cmake
+++ helpers/ice-cmake/cpp/cmake/ice_cpp_props.cmake
@@ -4,9 +4,27 @@
 set(ICE_BIN_DIST "")
 set(ICE_BUILDING_SRC 1) 
 if(WIN32)
+   set(ICE_COMPILE_DEFS 
+      "_CONSOLE"
+      "WIN32_LEAN_AND_MEAN"
+      "ICE_CPP11_MAPPING"
+   )
+
+   set(ICE_COMPILE_OPTIONS
+      "/W4"
+      "/wd4121"
+      "/wd4250"
+      "/wd4251"
+      "/wd4275"
+      "/wd4324"
+      "/bigobj"
+      "/MT"
+   )
+
+   set(CMAKE_RC_FLAGS "ICE_CPP11_MAPPING")
 
 elseif(DARWIN)
 
-elseif(UNIX)
+elseif(UNIX OR LINUX)
 
 endif()
