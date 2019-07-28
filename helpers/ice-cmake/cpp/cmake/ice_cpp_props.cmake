a051fa0ff2630818c9c8669f67a9d888d5aeb444
Nik Reist <auric.magi@gmail.com>
2019-07-27 20:28:25 -0400

    New script for OS specific compiler options

---
+++ helpers/ice-cmake/cpp/cmake/ice_cpp_props.cmake
@@ -0,0 +1,12 @@
+# define c++ compiler options and preprocessor definitions for all OS types
+
+# property flags 
+set(ICE_BIN_DIST "")
+set(ICE_BUILDING_SRC 1) 
+if(WIN32)
+
+elseif(DARWIN)
+
+elseif(UNIX)
+
+endif()
