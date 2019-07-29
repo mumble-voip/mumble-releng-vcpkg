1cbcb4efcf9ccdf0c96fafaf62c04a61ad2a115b
Nik Reist <auric.magi@gmail.com>
2019-07-29 17:55:02 -0400

    Add msvc condition, rename option var, add def

--- helpers/ice-cmake/cpp/cmake/ice_cpp_props.cmake
+++ helpers/ice-cmake/cpp/cmake/ice_cpp_props.cmake
@@ -2,26 +2,28 @@
 
 set(CMAKE_CXX_STANDARD 11) 
 if(WIN32)
-   set(ICE_COMPILE_DEFS 
-      "WIN32_LEAN_AND_MEAN"
-      "ICE_CPP11_MAPPING"
-   )
+   if(MSVC)
+      set(ICE_COMPILE_DEFS 
+         "WIN32_LEAN_AND_MEAN"
+         "ICE_CPP11_MAPPING"
+      )
 
-   set(ICE_COMPILE_OPTIONS
-      "/W4"
-      "/wd4121"
-      "/wd4250"
-      "/wd4251"
-      "/wd4275"
-      "/wd4324"
-      "/wd4127"
-      "/wd4505"
-      "/wd4512"
-      "/bigobj"
-      "/MT"
-   )
+      set(ICE_DIST_COMPILE_OPTIONS
+         "/W4"
+         "/wd4121"
+         "/wd4250"
+         "/wd4251"
+         "/wd4275"
+         "/wd4324"
+         "/wd4127"
+         "/wd4505"
+         "/wd4512"
+         "/bigobj"
+         "ICE_BUILDING_SOURCE"
+      )
 
-   set(ICE_RC_FLAGS "ICE_CPP11_MAPPING")
+      set(ICE_RC_FLAGS "ICE_CPP11_MAPPING")
+   endif()
 
 elseif(DARWIN)
 
