b03c3b99c29ea2a7f80bacff18f355dce9f164a3
Nik Reist <auric.magi@gmail.com>
2019-07-30 01:41:44 -0400

    Fix command, remove quotes, add c++11 support

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -8,12 +8,14 @@
 function(CompileSlices slice2_bin slice_path slice_folder_name slice_target output_dir)
    file(GLOB ice_files ${slice_path}/${slice_folder_name}/*.ice)
    foreach(ice ${ice_files})
-      get_filename_component(ice_FILE_NAME ${ice} NAME_WE)
-      add_custom_command(TARGET ${slice_target}
-         COMMAND "${slice2_bin} -I${slice_path} ${ice}"
-         WORKING_DIRECTORY ${output_dir}
-         DEPENDS ${ice_FILE_NAME}.ice
-         PRE_BUILD
-      )
+      if(NOT ice)
+         get_filename_component(ice_FILE_NAME ${ice} NAME_WE)
+         add_custom_command(TARGET ${slice_target}
+            COMMAND ${slice2_bin} -I${slice_path} ${ice} --impl-c++11
+            WORKING_DIRECTORY ${output_dir}
+            DEPENDS ${ice_FILE_NAME}.ice
+            PRE_BUILD
+         )
+      endif()
    endforeach()
 endfunction()
