56be48b5dc8976004f3939c45b478ad06fb603c9
Nik Reist <auric.magi@gmail.com>
2019-07-30 23:02:36 -0400

    Add c++std conditions and defs

--- helpers/ice-cmake/cpp/cmake/ice_cpp_props.cmake
+++ helpers/ice-cmake/cpp/cmake/ice_cpp_props.cmake
@@ -1,18 +1,16 @@
 # define c++ compiler options and preprocessor definitions for all OS types
 
-set(CMAKE_CXX_STANDARD 11) 
 if(WIN32)
    include("${PROJECT_SOURCE_DIR}/cpp/cmake/mc_compiler.cmake")
    if(MSVC)
-      set(ICE_COMPILE_DEFS
+      set(ICE_CPP98_COMPILE_DEFS
          "_CONSOLE" 
          "WIN32_LEAN_AND_MEAN"
          "ICE_BUILDING_SRC"
-         "_WIN32_WINNT"
       )
 
       if(CMAKE_CXX_STANDARD EQUAL 11)
-         set(ICE_COMPILE_DEFS "${ICE_COMPILE_DEFS}" "ICE_CPP11_MAPPING")
+         set(ICE_CPP11_COMPILE_DEFS "${ICE_CPP98_COMPILE_DEFS}" "ICE_CPP11_MAPPING")
          set(ICE_RC_FLAGS "ICE_CPP11_MAPPING")
       endif()
 
@@ -26,13 +24,13 @@ if(WIN32)
          "/wd4127"
          "/wd4505"
          "/wd4512"
-         "/bigobj"
          "/MP"
       )
       if(NOT BUILD_SHARED_LIBS)
          set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
          set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
-         set(ICE_COMPILE_DEFS "${ICE_COMPILE_DEFS}" "ICE_STATIC_LIBS")
+         set(ICE_CPP98_COMPILE_DEFS "${ICE_CPP98_COMPILE_DEFS}" "ICE_STATIC_LIBS")
+         set(ICE_CPP11_COMPILE_DEFS "${ICE_CPP11_COMPILE_DEFS}" "ICE_STATIC_LIBS")
       endif()
 
    endif()
