4fc094e722a15e30a089686cf79caf04a8202e40
Nik Reist <auric.magi@gmail.com>
2019-08-01 22:18:19 -0400

    Remove redundant data type description, comment

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -1,10 +1,10 @@
 # handle slice building so the projects that need it can call it
 # Types:
 # slice2_bin name_of_slice2_bin - compiled bin
-# slice_path string - root/slice
-# slice_dir string - name of folder (i.e. "Ice")
+# slice_path - root/slice
+# slice_dir - name of folder (i.e. "Ice")
 # slice_target - lib or exe this compile depends on
-# output_dir string - where compiled files will be output to
+# output_dir - where compiled files will be output to
 function(CompileSlices slice2_bin slice_path slice_dir slice_target output_dir lang_std)
    file(GLOB ice_files ${slice_path}/${slice_dir}/*.ice)
    foreach(ice ${ice_files})
