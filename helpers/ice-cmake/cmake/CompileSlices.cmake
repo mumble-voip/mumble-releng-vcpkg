d7a1ff6a5d1cf00cb631f41bd1d8910c45d61eb5
Nik Reist <auric.magi@gmail.com>
2019-08-01 22:17:15 -0400

    Shorten var for slice dir

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -2,11 +2,11 @@
 # Types:
 # slice2_bin name_of_slice2_bin - compiled bin
 # slice_path string - root/slice
-# slice_folder_name string - name of folder (i.e. "Ice")
+# slice_dir string - name of folder (i.e. "Ice")
 # slice_target - lib or exe this compile depends on
 # output_dir string - where compiled files will be output to
-function(CompileSlices slice2_bin slice_path slice_folder_name slice_target output_dir lang_std)
-   file(GLOB ice_files ${slice_path}/${slice_folder_name}/*.ice)
+function(CompileSlices slice2_bin slice_path slice_dir slice_target output_dir lang_std)
+   file(GLOB ice_files ${slice_path}/${slice_dir}/*.ice)
    foreach(ice ${ice_files})
          get_filename_component(ice_FILE_NAME ${ice} NAME_WE)
          add_custom_command(TARGET ${slice_target}
