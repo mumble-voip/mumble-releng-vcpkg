03d499f970859e4d38ae783666a506e9f9258551
Nik Reist <auric.magi@gmail.com>
2019-08-04 00:47:10 -0400

    Remove glob, handle in individual library files

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -5,14 +5,10 @@
 # slice_dir - name of folder (i.e. "Ice")
 # slice_target - lib or exe this compile depends on
 # output_dir - where compiled files will be output to
-function(CompileSlices slice2_bin slice_path slice_dir slice_target output_dir lang_std)
-   file(GLOB ice_files ${slice_path}/${slice_dir}/*.ice)
-   foreach(ice ${ice_files})
-         get_filename_component(ice_FILE_NAME ${ice} NAME_WE)
-         add_custom_command(TARGET ${slice_target}
-            COMMAND ${slice2_bin} -I${slice_path} ${ice} ${lang_std}
-            WORKING_DIRECTORY ${output_dir}
-            PRE_BUILD
-         )
-   endforeach()
+function(CompileSlices slice2_bin slice_path ice_file slice_target output_dir lang_std)
+   add_custom_command(TARGET ${slice_target}
+      COMMAND ${slice2_bin} -I${slice_path} ${ice_file} ${lang_std}
+      WORKING_DIRECTORY ${output_dir}
+      PRE_BUILD
+   )
 endfunction()
