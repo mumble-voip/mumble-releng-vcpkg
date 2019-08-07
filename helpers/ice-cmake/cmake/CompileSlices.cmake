f7bab2d55bf5d1f4f23d8aae0ab46db57b024dae
Nik Reist <auric.magi@gmail.com>
2019-08-06 22:53:25 -0400

    Remove function argument, not needed

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -5,7 +5,7 @@
 # slice_dir - name of folder (i.e. "Ice")
 # slice_target - lib or exe this compile depends on
 # output_dir - where compiled files will be output to
-function(CompileSlices slice2_bin slice_path ice_file slice_target output_dir lang_std)
+function(CompileSlices slice2_bin slice_path ice_file slice_target output_dir)
    add_custom_command(TARGET ${slice_target}
       COMMAND ${slice2_bin} -I${slice_path} ${ice_file}
       WORKING_DIRECTORY ${output_dir}
