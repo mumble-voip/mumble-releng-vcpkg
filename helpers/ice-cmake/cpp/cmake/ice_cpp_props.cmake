4a2c219c59a4ae68dbe2f612ccea77dcc8c4f746
Nik Reist <auric.magi@gmail.com>
2019-08-23 22:12:43 -0400

    Move non os specific defs out of conditions

--- helpers/ice-cmake/cpp/cmake/ice_cpp_props.cmake
+++ helpers/ice-cmake/cpp/cmake/ice_cpp_props.cmake
@@ -1,43 +1,49 @@
 # define c++ compiler options and preprocessor definitions for all OS types
+set(ICE_COMPILE_DEFS "ICE_BUILDING_SRC")
 
 if(WIN32)
-   if(MSVC)
-      set(ICE_CPP98_COMPILE_DEFS
-         "_CONSOLE" 
-         "WIN32_LEAN_AND_MEAN"
-         "ICE_BUILDING_SRC" 
-      )
+   set(ICE_CPP98_COMPILE_DEFS
+      "_CONSOLE" 
+      "WIN32_LEAN_AND_MEAN"
+      ${ICE_COMPILE_DEFS} 
+   )
 
-      if(CMAKE_CXX_STANDARD EQUAL 11)
-         set(ICE_CPP11_COMPILE_DEFS "${ICE_CPP98_COMPILE_DEFS}" "ICE_CPP11_MAPPING")
-      endif()
+   set(ICE_COMPILE_OPTIONS
+      "/W4"
+      "/wd4121"
+      "/wd4250"
+      "/wd4251"
+      "/wd4275"
+      "/wd4324"
+      "/wd4127"
+      "/wd4505"
+      "/wd4512"
+      "/MP"
+      "/bigobj"
+   )
 
-      set(ICE_COMPILE_OPTIONS
-         "/W4"
-         "/wd4121"
-         "/wd4250"
-         "/wd4251"
-         "/wd4275"
-         "/wd4324"
-         "/wd4127"
-         "/wd4505"
-         "/wd4512"
-         "/MP"
-         "/bigobj"
-      )
-      if(NOT BUILD_SHARED_LIBS)
-         set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
-         set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
-         set(ICE_CPP98_COMPILE_DEFS "${ICE_CPP98_COMPILE_DEFS}" "ICE_STATIC_LIBS")
-         if(CMAKE_CXX_STANDARD EQUAL 11)
-            set(ICE_CPP11_COMPILE_DEFS "${ICE_CPP11_COMPILE_DEFS}" "ICE_STATIC_LIBS")
-         endif()
-      endif()
+   set(ICE_ADDITIONAL_DEPS
+      "wsetargv.obj"
+   )
 
+   if(NOT BUILD_SHARED_LIBS)
+      set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
+      set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
    endif()
 
 elseif(DARWIN)
 
 elseif(UNIX OR LINUX)
 
+endif()
+
+if(CMAKE_CXX_STANDARD EQUAL 11)
+   set(ICE_CPP11_COMPILE_DEFS "${ICE_CPP98_COMPILE_DEFS}" "ICE_CPP11_MAPPING")
+endif()
+
+if(NOT BUILD_SHARED_LIBS)
+   set(ICE_CPP98_COMPILE_DEFS "${ICE_CPP98_COMPILE_DEFS}" "ICE_STATIC_LIBS")
+      if(CMAKE_CXX_STANDARD EQUAL 11)
+         set(ICE_CPP11_COMPILE_DEFS "${ICE_CPP11_COMPILE_DEFS}" "ICE_STATIC_LIBS")
+      endif()
 endif()
