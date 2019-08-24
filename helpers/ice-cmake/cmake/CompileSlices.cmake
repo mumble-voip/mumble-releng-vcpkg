4202ef5601f7aa748594015517910ae7d63beb72
Nik Reist <auric.magi@gmail.com>
2019-08-24 12:43:54 -0400

    Change to output driven for slice objects

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -5,11 +5,16 @@
 # slice_dir - name of folder (i.e. "Ice")
 # slice_target - lib or exe this compile depends on
 # output_dir - where compiled files will be output to
-function(CompileSlices slice2_bin slice_path ice_file slice_target output_dir)
-   add_custom_command(TARGET ${slice_target}
+function(CompileSlices slice2_bin slice_path ice_file slice_outputs)
+   if(CMAKE_BUILD_TYPE EQUAL DEBUG)
+      set(output_dir ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG})
+   elseif(CMAKE_BUILD_TYPE EQUAL RELEASE)
+      set(output_dir ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE})
+   endif()
+   add_custom_command(OUTPUT ${slice_outputs}
       COMMAND ${slice2_bin} -I${slice_path} ${ice_file}
       WORKING_DIRECTORY ${output_dir}
-      PRE_BUILD
+      #PRE_BUILD
       DEPENDS ${ice_file} ${slice2_bin}
    )
 endfunction()
