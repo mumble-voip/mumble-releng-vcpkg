00a0e774ccf28968615df7ce95ed045c43e27276
Nik Reist <auric.magi@gmail.com>
2019-08-24 21:36:03 -0400

    Remove, wrong usage

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -6,14 +6,9 @@
 # slice_target - lib or exe this compile depends on
 # output_dir - where compiled files will be output to
 function(CompileSlices slice2_bin slice_path ice_file slice_outputs)
-   if(CMAKE_BUILD_TYPE EQUAL DEBUG)
-      set(output_dir ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG})
-   elseif(CMAKE_BUILD_TYPE EQUAL RELEASE)
-      set(output_dir ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE})
-   endif()
    add_custom_command(OUTPUT ${slice_outputs}
       COMMAND ${slice2_bin} -I${slice_path} ${ice_file}
-      WORKING_DIRECTORY ${output_dir}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>
       #PRE_BUILD
       DEPENDS ${ice_file} ${slice2_bin}
    )
