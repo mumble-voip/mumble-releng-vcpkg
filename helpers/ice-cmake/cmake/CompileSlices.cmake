17dfd4aab740a78ad8dac4e2d0c1e0838b2019d0
Nik Reist <zeroability@outlook.com>
2019-08-24 22:16:55 -0400

    Fix output of srcs and whitespace

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -3,12 +3,12 @@
 # slice2_bin - name_of_slice2_bin (compiled bin)
 # slice_path - root/slice
 # slice_dir - name of folder (i.e. "Ice")
-# slice_target - lib or exe this compile depends on
-# output_dir - where compiled files will be output to
+# slice_outputs - source filename and types for the specific language
+
 function(CompileSlices slice2_bin slice_path ice_file slice_outputs)
    add_custom_command(OUTPUT ${slice_outputs}
       COMMAND ${slice2_bin} -I${slice_path} ${ice_file}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
       #PRE_BUILD
       DEPENDS ${ice_file} ${slice2_bin}
    )
