8dca53bacb3358059de907b372d473a0edbeec79
Nik Reist <zeroability@outlook.com>
2019-08-25 01:57:21 -0400

    Revert to target

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -5,10 +5,11 @@
 # slice_dir - name of folder (i.e. "Ice")
 # slice_outputs - source filename and types for the specific language
 # output_dir - specify the output dir for created sources
-function(CompileSlices slice2_bin slice_path ice_file slice_outputs output_dir)
-   add_custom_command(OUTPUT ${slice_outputs}
+function(CompileSlices slice2_bin slice_path ice_file slice_target output_dir)
+   add_custom_command(TARGET ${slice_target}
       COMMAND ${slice2_bin} -I${slice_path} ${ice_file}
       WORKING_DIRECTORY ${output_dir}
       DEPENDS ${ice_file} ${slice2_bin}
+      PRE_BUILD
    )
 endfunction()
