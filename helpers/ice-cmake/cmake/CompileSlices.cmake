6012c30b4127f34e304d5a96d4ea4037fdf0b0d6
Nik Reist <zeroability@outlook.com>
2019-08-25 00:33:55 -0400

    Add output dir var, remove option

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -4,12 +4,11 @@
 # slice_path - root/slice
 # slice_dir - name of folder (i.e. "Ice")
 # slice_outputs - source filename and types for the specific language
-
-function(CompileSlices slice2_bin slice_path ice_file slice_outputs)
+# output_dir - specify the output dir for created sources
+function(CompileSlices slice2_bin slice_path ice_file slice_outputs output_dir)
    add_custom_command(OUTPUT ${slice_outputs}
       COMMAND ${slice2_bin} -I${slice_path} ${ice_file}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-      #PRE_BUILD
+      WORKING_DIRECTORY ${output_dir}
       DEPENDS ${ice_file} ${slice2_bin}
    )
 endfunction()
