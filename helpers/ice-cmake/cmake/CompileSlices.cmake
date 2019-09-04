86056d3cd819335a879f582e042d4a6cf82addbe
Nik Reist <zeroability@outlook.com>
2019-09-03 22:12:04 -0400

    Add include_dir var for slice compiler, guard name

--- helpers/ice-cmake/cmake/CompileSlices.cmake
+++ helpers/ice-cmake/cmake/CompileSlices.cmake
@@ -4,10 +4,11 @@
 # slice_path - root/slice
 # slice_dir - name of folder (i.e. "Ice")
 # slice_outputs - source filename and types for the specific language
+# include_dir - needed to name generated header guards
 # output_dir - specify the output dir for created sources
-function(CompileSlices slice2_bin slice_path ice_file slice_target output_dir)
+function(CompileSlices slice2_bin slice_path ice_file slice_target include_dir output_dir)
    add_custom_command(TARGET ${slice_target}
-      COMMAND ${slice2_bin} -I${slice_path} ${ice_file}
+      COMMAND ${slice2_bin} -I${slice_path} ${ice_file} --include-dir ${include_dir}
       WORKING_DIRECTORY ${output_dir}
       DEPENDS ${ice_file} ${slice2_bin}
       PRE_BUILD
