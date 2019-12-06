b17f4678c26937cdc24d1ee7b5a213e857652394
Nik Reist <zeroability@outlook.com>
2019-12-06 01:18:03 -0500

    Modify for new msvc runtime lib setting

--- helpers/ice-cmake/cpp/cmake/ice_cpp_props.cmake
+++ helpers/ice-cmake/cpp/cmake/ice_cpp_props.cmake
@@ -27,8 +27,7 @@ if(WIN32)
     )
 
     if(NOT BUILD_SHARED_LIBS)
-        set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
-        set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
+        set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
     endif()
 
 elseif(APPLE)
