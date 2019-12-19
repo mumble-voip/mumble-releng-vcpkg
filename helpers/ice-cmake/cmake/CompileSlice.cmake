c5708c3e51a8f6251dfafa12426462e243e6ea47
Nik Reist <zeroability@outlook.com>
2019-12-19 18:21:40 -0500

    Rename CompileSlice file and function

--- helpers/ice-cmake/cmake/CompileSlice.cmake
+++ helpers/ice-cmake/cmake/CompileSlice.cmake
@@ -6,7 +6,7 @@
 # slice_outputs - source filename and types for the specific language
 # include_dir - needed to name generated header guards
 # output_dir - specify the output dir for created sources
-function(CompileSlices slice2_bin slice_path ice_file slice_target include_dir output_dir)
+function(CompileSlice slice2_bin slice_path ice_file slice_target include_dir output_dir)
     add_custom_command(TARGET ${slice_target}
         COMMAND ${slice2_bin} -I${slice_path} ${ice_file} --include-dir ${include_dir}
         WORKING_DIRECTORY ${output_dir}
