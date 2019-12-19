d3fe139a5dc283ebf117ebcccb5ba11e13312897
Nik Reist <zeroability@outlook.com>
2019-12-19 18:29:27 -0500

    Revise function for more customized parameters

--- helpers/ice-cmake/cmake/CompileSlice.cmake
+++ helpers/ice-cmake/cmake/CompileSlice.cmake
@@ -1,17 +1,16 @@
-# handle slice building so the projects that need it can call it
-# Types:
-# slice2_bin - name_of_slice2_bin (compiled bin)
-# slice_path - root/slice
-# slice_dir - name of folder (i.e. "Ice")
-# slice_outputs - source filename and types for the specific language
-# include_dir - needed to name generated header guards
+# CompileSlice - script slice compile with cmake similar to IceBuilder
+# 
+# Params:
+# slice2bin - name_of_slice2_bin (compiled bin)
+# slice2bin_params - pass paramaters to slice2bin as string
+#                    See params list here: https://doc.zeroc.com/ice/3.7/the-slice-language/using-the-slice-compilers
+# slice_target - cmake target (lib or bin) that is compiling the slice
 # output_dir - specify the output dir for created sources
-function(CompileSlice slice2_bin slice_path ice_file slice_target include_dir output_dir)
+function(CompileSlice slice2bin slice2bin_params slice_target output_dir)
     add_custom_command(TARGET ${slice_target}
-        COMMAND ${slice2_bin} -I${slice_path} ${ice_file} --include-dir ${include_dir}
+        COMMAND ${slice2_bin} ${slice2bin_params}
         WORKING_DIRECTORY ${output_dir}
-        DEPENDS ${ice_file} ${slice2_bin}
+        DEPENDS ${slice2_bin}
         PRE_BUILD
-        COMMENT "Generating sources for ${ice_file}..."
     )
 endfunction()
