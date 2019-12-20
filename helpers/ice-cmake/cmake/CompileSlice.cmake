4ae8b0df605dc0c766369bcb3efe10648c8812af
Nik Reist <zeroability@outlook.com>
2019-12-20 01:58:09 -0500

    Modify CompileSlice to handle global parameters

--- helpers/ice-cmake/cmake/CompileSlice.cmake
+++ helpers/ice-cmake/cmake/CompileSlice.cmake
@@ -5,12 +5,11 @@
 # slice_include_paths - include dir(s) needed for -I parameter
 # ice_file - file to be compiled (<filename>.ice)
 # slice_target - cmake target (lib or bin) that is compiling the slice
-# include_dir - name of the Ice project that is needed for the guard def
 # output_dir - specify the output dir for created sources
-function(CompileSlice slice2_bin slice_include_paths ice_file slice_target include_dir output_dir)
+function(CompileSlice slice2_bin slice2bin_params slice_include_paths ice_file slice_target output_dir)
     add_custom_command(TARGET ${slice_target}
         COMMAND ${slice2_bin} 
-        ARGS "--checksum" ${slice_include_paths} ${ice_file} "--include-dir" ${include_dir}
+        ARGS ${slice_include_paths} ${ice_file} ${slice2bin_params}
         WORKING_DIRECTORY ${output_dir}
         MAIN_DEPENDENCY ${ice_file}
         PRE_BUILD
