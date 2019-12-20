ad32ac90ae1edaf4027248a61505a4bae5e63aee
Nik Reist <zeroability@outlook.com>
2019-12-19 22:56:57 -0500

    Update function body and add lists for inc dirs

--- helpers/ice-cmake/cmake/CompileSlice.cmake
+++ helpers/ice-cmake/cmake/CompileSlice.cmake
@@ -1,16 +1,18 @@
 # CompileSlice - script slice compile with cmake similar to IceBuilder
 # 
 # Params:
-# slice2bin - name_of_slice2_bin (compiled bin)
-# slice2bin_params - pass paramaters to slice2bin as string
-#                    See params list here: https://doc.zeroc.com/ice/3.7/the-slice-language/using-the-slice-compilers
+# slice2_bin - name_of_slice2_bin (compiled bin)
+# slice_include_paths - include dir(s) needed for -I parameter
+# ice_file - file to be compiled (<filename>.ice)
 # slice_target - cmake target (lib or bin) that is compiling the slice
+# include_dir - name of the Ice project that is needed for the guard def
 # output_dir - specify the output dir for created sources
-function(CompileSlice slice2bin slice2bin_params slice_target output_dir)
+function(CompileSlice slice2_bin slice_include_paths ice_file slice_target include_dir output_dir)
     add_custom_command(TARGET ${slice_target}
-        COMMAND ${slice2_bin} ${slice2bin_params}
+        COMMAND ${slice2_bin} 
+        ARGS "--checksum" ${slice_include_paths} ${ice_file} "--include-dir ${include_dir}"
         WORKING_DIRECTORY ${output_dir}
-        DEPENDS ${slice2_bin}
+        MAIN_DEPENDENCY ${ice_file}
         PRE_BUILD
     )
 endfunction()
