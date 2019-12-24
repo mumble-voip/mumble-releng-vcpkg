c5175f13e375da19be44a3fa438e14babbe90662
Nik Reist <zeroability@outlook.com>
2019-12-24 14:47:35 -0500

    Fix debug include error, linux build

--- helpers/vcpkg/ports/mcpp/portfile.cmake
+++ helpers/vcpkg/ports/mcpp/portfile.cmake
@@ -28,4 +28,7 @@ file(
     DESTINATION 
         ${CURRENT_PACKAGES_DIR}/include
 )
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/mcpp RENAME copyright)
