a42f69b6ce4deb48ee43a26a47241a19ba06fb10
Nik Reist <zeroability@outlook.com>
2019-12-23 22:03:29 -0500

    Comment c++11 check, builds failing

--- helpers/vcpkg/ports/zeroc-ice/portfile.cmake
+++ helpers/vcpkg/ports/zeroc-ice/portfile.cmake
@@ -15,22 +15,30 @@ file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 # cpp11 mapping for static win32 builds is currently broken, build as cpp98
 
-if(WIN32 AND VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
-            -DCMAKE_CXX_STANDARD=98
-    )
+# if(WIN32 AND VCPKG_LIBRARY_LINKAGE STREQUAL static)
+#     vcpkg_configure_cmake(
+#         SOURCE_PATH ${SOURCE_PATH}
+#         OPTIONS
+#             -DCMAKE_CXX_STANDARD=98
+#     )
 
-else()
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
-            -DCMAKE_CXX_STANDARD=11
-    )
-endif()
+# else()
+#     vcpkg_configure_cmake(
+#         SOURCE_PATH ${SOURCE_PATH}
+#         OPTIONS
+#             -DCMAKE_CXX_STANDARD=11
+#     )
+# endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DCMAKE_CXX_STANDARD=98
+)
 
 vcpkg_install_cmake()
+
+vcpkg_copy_pdbs()
 
 file(INSTALL ${SOURCE_PATH}/cpp/include DESTINATION ${CURRENT_PACKAGES_DIR})
 file(INSTALL ${SOURCE_PATH}/slice DESTINATION ${CURRENT_PACKAGES_DIR})
