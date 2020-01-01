4d19282004b4134812a18b5d83aae7647af63bd8
Nik Reist <zeroability@outlook.com>
2019-12-31 21:14:09 -0500

    Change cmd signature, add dependencies

--- helpers/vcpkg/ports/zeroc-ice/cmake/CompileSlice.cmake
+++ helpers/vcpkg/ports/zeroc-ice/cmake/CompileSlice.cmake
@@ -2,16 +2,17 @@
 # 
 # Params:
 # slice2_bin - name_of_slice2_bin (compiled bin)
+# slice2bin_params - parameters passed for specific slice2_bin use cases
 # slice_include_paths - include dir(s) needed for -I parameter
-# ice_file - file to be compiled (<filename>.ice)
-# slice_target - cmake target (lib or bin) that is compiling the slice
+# ice_files - files to be compiled (<filename>.ice)
 # output_dir - specify the output dir for created sources
-function(CompileSlice slice2_bin slice2bin_params slice_include_paths ice_file slice_target output_dir)
-    add_custom_command(TARGET ${slice_target}
+function(CompileSlice slice2_bin slice2bin_params slice_include_paths ice_file output_dir output_file)
+    add_custom_command(OUTPUT ${output_file}
         COMMAND ${slice2_bin} 
         ARGS ${slice_include_paths} ${ice_file} ${slice2bin_params}
         WORKING_DIRECTORY ${output_dir}
-        MAIN_DEPENDENCY ${ice_file}
-        PRE_BUILD
+        MAIN_DEPENDENCY ${slice2_bin}
+        DEPENDS ${ice_file}
+        COMMENT "Compiling sources for ${ice_file}..."
     )
 endfunction()
