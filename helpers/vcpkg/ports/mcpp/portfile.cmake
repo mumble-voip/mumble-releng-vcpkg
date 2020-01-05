50c297f36ab7a384c490566586525f53c61f8fc3
Nik Reist <zeroability@outlook.com>
2020-01-05 17:36:03 -0500

    Remove linkage check, defined in project

--- helpers/vcpkg/ports/mcpp/portfile.cmake
+++ helpers/vcpkg/ports/mcpp/portfile.cmake
@@ -10,8 +10,6 @@ vcpkg_from_github(
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
