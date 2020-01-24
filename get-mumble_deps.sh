429ab021bff97ef5fe0daf571b97df5044e5a920
Nik Reist <zeroability@outlook.com>
2020-01-24 08:39:44 -0500

    Update copyright date

--- get-mumble_deps.sh
+++ get-mumble_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2018 The 'mumble-releng-experimental' Authors. All rights reserved.
+# Copyright 2020 The 'mumble-releng-experimental' Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that
 # can be found in the LICENSE file in the source tree or at
 # <http://mumble.info/mumble-releng-experimental/LICENSE>.
