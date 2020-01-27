0a6f0b72cb055f9f841e76ac7a38345c67c37138
Nik Reist <zeroability@outlook.com>
2020-01-27 01:49:42 -0500

    Move depenencies to a list, add a for

--- get-mumble_deps.sh
+++ get-mumble_deps.sh
@@ -5,6 +5,23 @@
 # can be found in the LICENSE file in the source tree or at
 # <http://mumble.info/mumble-releng-experimental/LICENSE>.
 
+mumble_deps='qt5-base, 
+            qt5-svg, 
+            qt5-tools, 
+            grpc, 
+            boost-accumulators, 
+            boost-atomic, 
+            boost-function, 
+            boost-optional, 
+            boost-system, 
+            boost-thread, 
+            libvorbis, 
+            libogg, 
+            libflac, 
+            sndfile, 
+            libmariadb, 
+            zeroc-ice'
+
 case "$OSTYPE" in
     msys* ) triplet='x64-windows-static-md';;
     linux-gnu* ) triplet='x64-linux';;
@@ -34,9 +51,13 @@ if [ ! -d "~/vcpkg" ]
 			    then
 			        echo "Triplet type is not defined! Aborting..."
 			else
-                ./vcpkg install qt5-base qt5-svg qt5-tools grpc boost-atomic \
-                    boost-function boost-optional boost-system boost-thread libvorbis \
-                    boost-accumulators libogg libflac sndfile libmariadb zeroc-ice --triplet $triplet
+                # ./vcpkg install qt5-base qt5-svg qt5-tools grpc boost-atomic \
+                #     boost-function boost-optional boost-system boost-thread libvorbis \
+                #     boost-accumulators libogg libflac sndfile libmariadb zeroc-ice --triplet $triplet
+                for dep in ${mumble_deps//,/ }
+                do
+                    ./vcpkg install $dep:$triplet
+                done
 			fi
     else
         echo "Failed to retrieve the 'vcpkg' repository! Aborting..."
