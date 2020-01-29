93f8576d525b01acdd91e485d757b9ed8a720431
Nik Reist <zeroability@outlook.com>
2020-01-28 21:04:27 -0500

    Copy ports into new "community" path

--- get-mumble_deps.sh
+++ get-mumble_deps.sh
@@ -37,7 +37,7 @@ if [ ! -d "~/vcpkg" ]
             # vcpkg does not have a port for zeroc ice or mcpp, copy homegrown ports 
             cp -R helpers/vcpkg/ports/* ~/vcpkg/ports/
             # copy custom triplet files
-            cp helpers/vcpkg/triplets/* ~/vcpkg/triplets/
+            cp helpers/vcpkg/triplets/* ~/vcpkg/triplets/community
             cd ~/vcpkg
             case "$OSTYPE" in
                 msys* ) ./bootstrap-vcpkg.bat -disableMetrics
