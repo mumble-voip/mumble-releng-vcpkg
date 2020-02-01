2519c8cfcec367623305c9cfc7c098055e27f772
Nik Reist <zeroability@outlook.com>
2020-01-31 22:01:26 -0500

    Revise function to use available slice compiler

--- helpers/vcpkg/ports/zeroc-ice/cmake/CompileSlice.cmake
+++ helpers/vcpkg/ports/zeroc-ice/cmake/CompileSlice.cmake
@@ -1,21 +1,20 @@
 # CompileSlice - script slice compile with cmake similar to IceBuilder
 # 
 # Params:
-# slice2_bin - name_of_slice2_bin (compiled bin)
 # slice2bin_params - parameters passed for specific slice2_bin use cases
 # slice_include_paths - include dir(s) needed for -I parameter
-# ice_files - files to be compiled (<filename>.ice)
+# ice_file - file to be compiled (<filename>.ice)
 # output_dir - generated source output path
 # output_file - generated source output file name
 
-function(CompileSlice slice2_bin slice2bin_params slice_include_paths ice_file output_dir output_file)
+function(CompileSlice slice2bin_params slice_include_paths ice_file output_dir output_file)
 	add_custom_command(OUTPUT ${output_file}
-		COMMAND $<TARGET_FILE:${slice2_bin}> 
+		COMMAND ${SLICE2_BIN}
 		ARGS ${slice_include_paths} ${ice_file} ${slice2bin_params}
 		BYPRODUCTS ${output_file}
 		WORKING_DIRECTORY ${output_dir}
 		MAIN_DEPENDENCY ${ice_file}
-		DEPENDS ${slice2_bin}
+		DEPENDS ${SLICE2_BIN}
 		COMMENT "Generating sources for ${ice_file}..."
 	)
 endfunction()
