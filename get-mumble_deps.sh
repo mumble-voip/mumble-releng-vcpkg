3d8f7cd9b0be799d9dbfce83c2f0cfce150c3a48
Jan Klass <kissaki@posteo.de>
2020-02-02 20:54:36 +0100

    Make get-mumble_deps.sh more configurable

--- get-mumble_deps.sh
+++ get-mumble_deps.sh
@@ -5,6 +5,9 @@
 # can be found in the LICENSE file in the source tree or at
 # <http://mumble.info/mumble-releng-experimental/LICENSE>.
 
+VCPKGDIR=${VCPKGDIR:-~/vcpkg}
+VCPKG_INTEGRATE=${VCPKG_INTEGRATE:-1}
+triplet=
 mumble_deps='qt5-base, 
             qt5-svg, 
             qt5-tools, 
@@ -23,46 +26,95 @@ mumble_deps='qt5-base,
             zlib, 
             zeroc-ice'
 
-case "$OSTYPE" in
-    msys* ) triplet='x64-windows-static-md';;
-    linux-gnu* ) triplet='x64-linux';;
-    darwin* ) triplet='x64-osx';;
-    * ) echo "The OSTYPE is either not defined or unsupported. Aborting...";;
-esac
+if [ triplet == "" ]; then
+    case "$OSTYPE" in
+        msys* ) triplet='x64-windows-static-md';;
+        linux-gnu* ) triplet='x64-linux';;
+        darwin* ) triplet='x64-osx';;
+        * )
+            echo "The OSTYPE is either not defined or unsupported. Could not determine tripled. Aborting..."
+            exit 1
+            ;;
+    esac
+fi
 
-if [ ! -d "~/vcpkg" ]
-    then 
-        git clone https://github.com/Microsoft/vcpkg.git ~/vcpkg
-    if [ "$(ls -A ~/vcpkg)" ] 
-        then
-            # vcpkg does not have a port for zeroc ice or mcpp, copy homegrown ports 
-            cp -R helpers/vcpkg/ports/* ~/vcpkg/ports/
-            # copy custom triplet files
-            cp helpers/vcpkg/triplets/* ~/vcpkg/triplets/community
-            cd ~/vcpkg
-            case "$OSTYPE" in
-                msys* ) ./bootstrap-vcpkg.bat -disableMetrics
-                        ./vcpkg integrate install
-                        # install dns-sd provider
-                        ./vcpkg install mdnsresponder:$triplet;;
-                * ) bash bootstrap-vcpkg.sh
-                    ./vcpkg integrate install;;
-            esac
-            if [ -z "$triplet" ]
-                then
-                echo "Triplet type is not defined! Aborting..."
-            else
-                for dep in ${mumble_deps//,/ }
-                do
-                    ./vcpkg install $dep:$triplet
-                done
-            fi
-    else
-        echo "Failed to retrieve the 'vcpkg' repository! Aborting..."
+function prepVcpkg {
+    TARGET=$1
+    INTEGRATE=${2:-0}
+    if [ ! -d "$TARGET" ]; then
+        git clone https://github.com/Microsoft/vcpkg.git $TARGET
+        if [ ! $? -eq 0 ]; then
+            echo "Failed to clone 'vcpkg'. Aborting..."
+            return 1
+        fi
+
+        # vcpkg does not have a port for zeroc ice or mcpp, copy homegrown ports 
+        cp -R helpers/vcpkg/ports/* $TARGET/ports/
+        # copy custom triplet files
+        cp helpers/vcpkg/triplets/* $TARGET/triplets/community
+
+        pushd $TARGET
+        case "$OSTYPE" in
+            msys* )
+                ./bootstrap-vcpkg.bat -disableMetrics
+                if [ $INTEGRATE = 1 ]; then
+                    ./vcpkg integrate install
+                fi
+                # install dns-sd provider
+                ./vcpkg install mdnsresponder:$triplet
+                ;;
+            * )
+                bash bootstrap-vcpkg.sh
+                if [ $INTEGRATE = 1 ]; then
+                    ./vcpkg integrate install
+                fi
+                ;;
+        esac
+        popd
+
+        if [ ! $? -eq 0 ]; then
+            echo "Failed to prepare 'vcpkg'. Aborting..."
+            exit 1
+        fi
+
+        return 0
     fi
-else
+    echo Using existing vcpkg in $TARGET
+    return 0
+}
+
+function prepDeps {
+    pushd $VCPKGDIR
+
+    if [ $VCPKG_INTEGRATE = 0 ]; then
+        case "$OSTYPE" in
+            msys* ) ./bootstrap-vcpkg.bat;;
+            * ) bash bootstrap-vcpkg.sh;;
+        esac
+    fi
+
+    if [ -z "$triplet" ]; then
+        echo "Triplet type is not defined! Aborting..."
+        return 1
+    fi
     for dep in ${mumble_deps//,/ }
     do
-        ./vcpkg install $dep:$triplet
+        ./vcpkg install $dep:${triplet:?required triplet variable is not set}
+        if [ ! $? -eq 0 ]; then
+            popd
+            echo "Failed to prepare dependency '$dep:$triplet'. Aborting..."
+            exit 1
+        fi
     done
+
+    popd
+    return 0
+}
+
+prepVcpkg $VCPKGDIR $VCPKG_INTEGRATE
+if [ ! $? -eq 0 ]; then
+    echo "Failed to prepare 'vcpkg'. Aborting..."
+    exit 1
 fi
+
+prepDeps
